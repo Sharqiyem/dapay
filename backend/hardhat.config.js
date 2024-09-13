dc18b50ce1be554dade6621c7acd4c07873010c6
Salam <sharqiyem@yandex.ru>
2024-09-14 02:00:35 +0800

    add sepolia testnet

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -1,6 +1,25 @@
 require('@nomicfoundation/hardhat-toolbox');
 
 /** @type import('hardhat/config').HardhatUserConfig */
+require("dotenv").config();
+
+const SEPOLIA_PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY;
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+
 module.exports = {
-  solidity: '0.8.24',
+  solidity: "0.8.24",
+  networks: {
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accounts: [SEPOLIA_PRIVATE_KEY]
+    }
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+
+  },
+  sourcify: {
+    enabled: true
+  }
 };
--- webapp/constants/contract.ts
+++ webapp/constants/contract.ts
@@ -1,6 +1,6 @@
-export const contractAddress = '0x36C02dA8a0983159322a80FFE9F24b1acfF8B570';
+export const contractAddress = "0xe9D99Ed4A2d9EaF675bB362128C21a463eC9acDE";
 //Second:  0x70997970C51812dc3A010C7d01b50e0d17dc79C8
 
 //First:  0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266
 
-export const initialFeePercentage = 100;
+export const initialFeePercentage = 100
