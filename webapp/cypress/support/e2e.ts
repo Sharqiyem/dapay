226f34314ac1cebb451cf83a8ab1119cadc4067b
Salam <sharqiyem@yandex.ru>
2024-09-14 05:19:31 +0800

    coment cypress

--- webapp/cypress/support/e2e.ts
+++ webapp/cypress/support/e2e.ts
@@ -1,11 +1,11 @@
-import '@testing-library/cypress/add-commands';
+// import '@testing-library/cypress/add-commands';
 
-// Prevent TypeScript from reading file as legacy script
-export {};
+// // Prevent TypeScript from reading file as legacy script
+// export {};
 
-Cypress.on('uncaught:exception', (err) => {
-  // Returning false here prevents Cypress from failing the test
-  if (err.message.includes('ResizeObserver loop limit exceeded')) {
-    return false;
-  }
-});
+// Cypress.on('uncaught:exception', (err) => {
+//   // Returning false here prevents Cypress from failing the test
+//   if (err.message.includes('ResizeObserver loop limit exceeded')) {
+//     return false;
+//   }
+// });
